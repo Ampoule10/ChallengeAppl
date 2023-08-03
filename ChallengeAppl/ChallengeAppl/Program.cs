6402294db9bbca6f4ab3493ccd92995ae446e51e
Ampoule10 <ampoule10@gmail.com>
2023-08-03 12:17:02 +0200

    Zadanie domowe - Dzień 4

--- ChallengeAppl/ChallengeAppl/Program.cs
+++ ChallengeAppl/ChallengeAppl/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string gender = "woman";
+int age = 33;
+
+if (age < 30 && gender == "woman")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && gender != "woman")
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
