48593737a7abd1c6ffd2f388d8120fb9a4752454
Ampoule10 <ampoule10@gmail.com>
2023-08-04 18:46:01 +0200

    Zadanie domowe - Dzień 9

--- ChallengeAppl/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeAppl/ChallengeApp.Tests/EmployeeTests.cs
@@ -1,54 +1,27 @@
-namespace ChallengeAppl.Tests
+﻿namespace ChallengeAppl.Tests
 {
-    public class Tests
-    {
+    public class EmployeeTests
+    {      
 
         [Test]
-        public void WhenEmployee1ScoreSum_ShouldReturnCorrectResult()
+        public void WhenEmployeeCollectGrades_ShouldCorrectResult()
         {
             //arrange
-            var employee1 = new Employee("Marcin", "Nowak", 21);
-            employee1.AddScore(6);
-            employee1.AddScore(3);
-            employee1.AddScore(-8);
+            var employee = new Employee("Robert", "Lewandowski");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
 
             //act
-            var result1 = employee1.Result;
+            var result = employee.GetStatistics();
 
             //assert
-            Assert.AreEqual(1, result1);
-        }
-        [Test]
-
-        public void WhenEmployee2ScoreSum_ShouldReturnCorrectResult()
-        {
-            //arrange
-            var employee2 = new Employee("Stefan", "Kowalski", 30);
-            employee2.AddScore(8);
-            employee2.AddScore(5);
-            employee2.AddScore(-8);
-
-            //act
-            var result2 = employee2.Result;
-
-            //assert
-            Assert.AreEqual(5, result2);
-
-        }
-        [Test]
-        public void WhenEmployee3ScoreSum_ShouldReturnCorrectResult()
-        {
-            //arrange
-            var employee3 = new Employee("Marek", "Budzisz", 27);
-            employee3.AddScore(9);
-            employee3.AddScore(5);
-            employee3.AddScore(-2);
-
-            //act
-            var result3 = employee3.Result;
-
-            //assert
-            Assert.AreEqual(12, result3);
-        }
+            Assert.That(result.Average, Is.EqualTo(3.33f).Within(0.01f));
+            Assert.That(result.Min, Is.EqualTo(2));
+            Assert.That(result.Max, Is.EqualTo(6));
+        }  
     }
 }
+
+
+
--- ChallengeAppl/ChallengeAppl/Program.cs
+++ ChallengeAppl/ChallengeAppl/Program.cs
@@ -1,43 +1,14 @@
 ﻿using ChallengeAppl;
 
-Employee employee1 = new Employee("Marcin", "Nowak", 21);
-Employee employee2 = new Employee("Stefan", "Kowalski", 30);
-Employee employee3 = new Employee("Marek", "Budzisz", 27);
+Employee employee = new Employee("Robert", "Lewandowski");
+employee.AddGrade(2);
+employee.AddGrade(2);
+employee.AddGrade(6);
 
+var statistics = employee.GetStatistics();
 
-employee1.AddScore(9);
-employee1.AddScore(8);
-employee1.AddScore(7);
-employee1.AddScore(6);
-employee1.AddScore(5);
+Console.WriteLine($"Wyniki dla: {employee.Name} {employee.Surname}:");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min:N2}");
+Console.WriteLine($"Max: {statistics.Max:N2}");
 
-employee2.AddScore(8);
-employee2.AddScore(7);
-employee2.AddScore(6);
-employee2.AddScore(5);
-employee2.AddScore(4);
-
-employee3.AddScore(9);
-employee3.AddScore(8);
-employee3.AddScore(9);
-employee3.AddScore(8);
-employee3.AddScore(9);
-
-List<Employee> employees = new List<Employee>()
-{
-   employee1 ,employee2 , employee3
-};
-
-int maxResult = -1;
-Employee employeeWithMaxResult = null;
-
-foreach (var employee in employees)
-{
-    if (employee.Result > maxResult)
-    {
-        employeeWithMaxResult = employee;
-        maxResult = employeeWithMaxResult.Result;
-    }
-
-}
-Console.WriteLine("Pracownik - " + employeeWithMaxResult.Name + " " + "" + employeeWithMaxResult.Surname + ", " + "" + (employeeWithMaxResult.Age + " lat, ") + "" + "uzyskał największą sumę ocen: " + "" + employeeWithMaxResult.Result);
